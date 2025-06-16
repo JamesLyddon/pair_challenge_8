7d6c5527d2f0448feccb50f5850e44158523bd8f
james lyddon <jameslyddon@gmail.com>
2025-06-16 12:56:31 +0100

    jims refacotr

--- lib/friendlist.py
+++ lib/friendlist.py
@@ -1,41 +1,47 @@
 from datetime import datetime, timedelta
 
-
 class FriendList:
     def __init__(self):
         self.friends = []
 
-    def add_friend(self, name, dob):
+    # helper functions
+    def validate_name(self, name):
+        if any(friend['name'].lower() == name.lower() for friend in self.friends):
+            raise ValueError(f"You already have a friend named {name.title()}!")
+        return name.title()
+
+    def validate_dob(self, dob):
         try:
-            dob_as_datetime = datetime.strptime(dob, "%Y-%m-%d")
-        except Exception:
-            raise Exception('dob must be in "YYYY-MM-DD format"')
-        self.friends.append({'name': name, 'dob': dob_as_datetime})
+            return datetime.strptime(dob, "%Y-%m-%d")
+        except ValueError:
+            raise ValueError('dob must be in "YYYY-MM-DD format"')
+
+    def calculate_age_on(self, dob, date):
+        return date.year - dob.year
+
+    def next_birthday(self, dob):
+        today = datetime.now()
+        bday = dob.replace(year=today.year)
+        if bday < today:
+            bday = bday.replace(year=today.year + 1)
+        return bday
+
+    # CRUD functions
+    def add_friend(self, name, dob):
+        self.friends.append({'name': self.validate_name(name), 'dob': self.validate_dob(dob)})
 
     def update_dob(self, name, dob):
-        updated_friends = []
-        for friend in self.friends:
-            if friend["name"] == name:
-                updated_friends.append({**friend, "dob": dob})
-            else:
-                updated_friends.append(friend)
-        self.friends = updated_friends
+        self.friends = [{**friend, 'dob': self.validate_dob(dob)} if friend['name'] == name else friend for friend in self.friends]
 
     def update_name(self, old_name, new_name):
-        updated_friends = []
-        for friend in self.friends:
-            if friend["name"] == old_name:
-                updated_friends.append({**friend, "name": new_name})
-            else:
-                updated_friends.append(friend)
-        self.friends = updated_friends
+        self.friends = [{**friend, 'name': new_name} if friend['name'] == old_name else friend for friend in self.friends]
 
     def upcoming_birthdays(self, days_in_advance):
-        upcoming_friends_birthdays = []
         today = datetime.now()
+        end_date = today + timedelta(days_in_advance)
+        upcoming = []
         for friend in self.friends:
-            upcoming_birthday = friend["dob"].replace(year=datetime.now().year)
-            if upcoming_birthday - today <= timedelta(days_in_advance):
-                upcoming_age = (today.year - friend["dob"].year)
-                upcoming_friends_birthdays.append({**friend, "upcoming_age": upcoming_age})
-        return upcoming_friends_birthdays
+            bday_this_year = self.next_birthday(friend['dob'])
+            if today <= bday_this_year <= end_date:
+                upcoming.append({**friend, "upcoming_age": self.calculate_age_on(friend['dob'], bday_this_year)})
+        return upcoming
--- tests/friendlist_unit_test.py
+++ tests/friendlist_unit_test.py
@@ -11,20 +11,16 @@ def test_initialisation():
 Given a name and dob
 We can add a friend to our friends list
 """
-
-
 def test_add_friend():
     fl = FriendList()
     fl.add_friend('Bill', '1990-08-13')
     dob_as_datetime = datetime.strptime('1990-08-13', "%Y-%m-%d")
     assert fl.friends[0] == {'name': 'Bill', 'dob': dob_as_datetime}
 
-# """
-# Given a name and incorrect dob
-# We raise an exception with the message 'dob must be in "YYYY-MM-DD format"'
-# """
-
-
+"""
+Given a name and incorrect dob
+We raise an exception with the message 'dob must be in "YYYY-MM-DD format"'
+"""
 def test_add_friend_incorrect_dob_raises_exception():
     with pytest.raises(Exception) as e:
         fl = FriendList()
@@ -32,49 +28,55 @@ def test_add_friend_incorrect_dob_raises_exception():
         datetime.strptime('13-08-13', "%Y-%m-%d")
     assert str(e.value) == 'dob must be in "YYYY-MM-DD format"'
 
-# """
-# Given a new dob
-# We can update an existing friend's dob
-# """
-# friendlist = FriendList()
-# friendlist.add_friend('Bill', '1990-08-13')
-# friendlist.update_dob('Bill', '1990-09-13')
-# friendlist.friends[0] # => {'name': 'Bill', 'dob': '1990-09-13'}
+"""
+Given a FriendList
+We raise a ValueError when adding a name that already exists
+"""
+def test_adding_existing_name():
+    friendlist = FriendList()
+    friendlist.add_friend('Bill', '1990-08-13')
+    with pytest.raises(Exception) as e:
+        friendlist.add_friend('bill', '1990-10-09')
+    err_msg = str(e.value)
+    assert err_msg == "You already have a friend named Bill!"
 
-
+"""
+Given a new dob
+We can update an existing friend's dob
+"""
 def test_update_dob():
     friendlist = FriendList()
     friendlist.add_friend('Bill', '1990-08-13')
     friendlist.update_dob('Bill', '1990-09-13')
-    assert friendlist.friends[0] == {'name': 'Bill', 'dob': '1990-09-13'}
+    dob = datetime.strptime('1990-09-13', "%Y-%m-%d")
+    assert friendlist.friends[0] == {'name': 'Bill', 'dob': dob}
 
-# """
-# Given a new name
-# We can update an existing friend's name
-# """
-# friendlist = FriendList()
-# friendlist.add_friend('Bill', '1990-08-13')
-# friendlist.update_name('Bill', 'Phil')
-# friendlist.friends[0] # => {'name': 'Phil', 'dob': '1990-08-13'}
+"""
+Given a bad dob
+We can't update an existing friend's dob but get an exception
+"""
+def test_update_bad_dob():
+    friendlist = FriendList()
+    friendlist.add_friend('Bill', '1990-08-13')
+    with pytest.raises(Exception) as e:
+        friendlist.update_dob('Bill', '1990,09,13')
+    err_msg = str(e.value)
+    assert err_msg == 'dob must be in "YYYY-MM-DD format"'
 
-
+"""
+Given a new name
+We can update an existing friend's name
+"""
 def test_update_name():
     friendlist = FriendList()
     friendlist.add_friend('Bill', '1990-08-13')
     friendlist.update_name('Bill', "Phil")
     assert friendlist.friends[0] == {'name': 'Phil', 'dob': friendlist.friends[0]["dob"]}
 
-# """
-# Given a FriendList
-# We can request upcoming birtdays in the following 'x' days
-# """
-# friendlist = FriendList()
-# friendlist.add_friend('Bill', '1990-08-13')
-# friendlist.add_friend('Bob', '1990-06-17')
-# friendlist.add_friend('Jeff', '1990-11-20')
-# friendlist.upcoming_birthdays(30) # => [{'name': 'Bob', 'dob': '1990-06-17', 'upcoming_age': 35}]
-
-
+"""
+Given a FriendList
+We can request upcoming birtdays in the following 'x' days
+"""
 def test_upcoming_birthdays():
     friendlist = FriendList()
     friendlist.add_friend('Bill', '1990-08-13')
